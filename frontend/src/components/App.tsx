85a395461db05ee88921d01126acd938f48acd82
Ahmed Al-Jawahiry <ahmed.code.1293@gmail.com>
2020-06-28 02:36:24 +0100

    add team themes

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -6,21 +6,21 @@ import Spinner from "./Spinner";
 
 export default function App() {
 
-    return <div className='text-gray-400 font-sans text-center h-full'>
+    return <div className='text-gray-400 font-sans h-full'>
 		 <div className='grid grid-cols-2 divide-y divide-gray-200 divide-opacity-25 row-gap-5'>
-			 <div className='col-span-2'>
+			 <div className='col-span-2 text-center'>
 				 <h1 className='text-4xl mt-4'>_football</h1>
 			 </div>
-			 <div className='col-span-2'>
+			 <div className='col-span-2 flex justify-center'>
 				 Buttons...
 			 </div>
-			 <div className='col-span-2'>
+			 <div className='col-span-2 flex justify-center'>
 				 Search...
 			 </div>
-			 <div className='col-span-1'>
+			 <div className='col-span-1 flex justify-center'>
 				 <Spinner/>
 			 </div>
-			 <div className='col-span-1'>
+			 <div className='col-span-1 flex justify-center'>
 				 <LeagueTable api={fetchPLTable} highlightedTeams={ALL_TEAMS}/>
 			 </div>
 		 </div>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+    	colors: {
+    		'arsenal-home': '#DB0007',
+			'arsenal-text': '#fff',
+			'thfc-home': '#fff',
+			'thfc-text': '#132257',
+			'lfc-home': '#C8102E',
+			'lfc-text': '#F6EB61',
+			'mcfc-home': '#6CABDD',
+			'mcfc-text': '#fff',
+			'mufc-home': '#DA291C',
+			'mufc-text': '#000000',
+			'chelsea-home': '#034694',
+			'chelsea-text': '#fff',
+			'norwich-home': '#00a650',
+			'norwich-text': '#fff200'
+		}
+	 },
+  },
+  variants: {},
+  plugins: [],
+}
