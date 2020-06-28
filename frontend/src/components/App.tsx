5ead416183b32df2690eec37c66e8eb9e2effde3
Ahmed Al-Jawahiry <ahmed.code.1293@gmail.com>
2020-06-28 02:20:08 +0100

    setup grid

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -2,10 +2,27 @@ import React from 'react';
 import LeagueTable from "./LeagueTable";
 import {fetchPLTable} from "../services/football";
 import {ALL_TEAMS} from "../util/constants";
+import Spinner from "./Spinner";
 
 export default function App() {
 
-    return <div className='text-gray-400 font-sans'>
-        <LeagueTable api={fetchPLTable} highlightedTeams={ALL_TEAMS}/>
+    return <div className='text-gray-400 font-sans text-center h-full'>
+		 <div className='grid grid-cols-2 divide-y divide-gray-200 divide-opacity-25 row-gap-5'>
+			 <div className='col-span-2'>
+				 <h1 className='text-4xl mt-4'>_football</h1>
+			 </div>
+			 <div className='col-span-2'>
+				 Buttons...
+			 </div>
+			 <div className='col-span-2'>
+				 Search...
+			 </div>
+			 <div className='col-span-1'>
+				 <Spinner/>
+			 </div>
+			 <div className='col-span-1'>
+				 <LeagueTable api={fetchPLTable} highlightedTeams={ALL_TEAMS}/>
+			 </div>
+		 </div>
     </div>
 }
--- frontend/src/components/Spinner.tsx
+++ frontend/src/components/Spinner.tsx
@@ -2,5 +2,5 @@ import React from "react";
 
 
 export default function Spinner() {
-	return <div className='spinner' data-testid='spinner'/>
+	return <div className='spinner m-5' data-testid='spinner'/>
 }
