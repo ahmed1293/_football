b7652ce1ba92198fc0fedd8821bcf0c615cd2898
Ahmed Al-Jawahiry <ahmed.code.1293@gmail.com>
2020-06-28 17:03:58 +0100

    add search and all/none btns

---
+++ frontend/src/components/Search.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export default function SearchInput() {
+
+	return <div className='mt-5 w-1/2'>
+		<input
+			className="shadow appearance-none border rounded w-full py-2 px-3 bg-black
+			text-gray-300 leading-tight focus:outline-none focus:outline-none"
+			type="text"
+			placeholder="Search..."
+		/>
+	</div>
+
+}
